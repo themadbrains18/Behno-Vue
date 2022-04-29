4b1cf58db1522bd1a81a60047259f3e33ff85825
“vipan1890” <vipin.mdb@gmail.com>
2022-04-29 20:18:52 +0530

    update popup functionalities

--- src/vue/components/render/blog/img-column-full-bleed-v2.vue
+++ src/vue/components/render/blog/img-column-full-bleed-v2.vue
@@ -2,13 +2,19 @@
     <section class="img_column_full_bleed_sec bg-sand">
         <div class="sec_sontent">
             <div class="sec_img">
-                <img src="https://cdn.shopify.com/s/files/1/0577/1178/8125/files/image-column1.jpg?v=1651053964" alt="error">
-                <div class="shop_cta">
+                <div class="product_popup">
+                    <img src="https://cdn.shopify.com/s/files/1/0577/1178/8125/files/image-column1.jpg?v=1651053964" alt="error">
+                    <productPopup />
+                </div>
+                    <div class="shop_cta">
                     <!-- <a href="#" class="subtitle">SHOP THE BAG</a> -->
                 </div>
             </div>
             <div class="sec_img">
-                <img src="https://cdn.shopify.com/s/files/1/0577/1178/8125/files/image-column2.jpg?v=1651053964" alt="error">
+                <div class="product_popup">
+                    <img src="https://cdn.shopify.com/s/files/1/0577/1178/8125/files/image-column2.jpg?v=1651053964" alt="error">
+                    <productPopup />
+                </div>
                 <div class="shop_cta">
                     <a href="#" class="subtitle">SHOP THE BAG</a>
                 </div>
@@ -55,3 +61,61 @@
         }
     }
 </style>
+
+<script>
+import productPopup from "../product-popup.vue";
+
+export default {
+  components: {
+    productPopup,
+  },
+  data() {
+    //    this.closePopup();
+    window.addEventListener("load", this.showPopup);
+    window.addEventListener("load", this.closePopup);
+    window.addEventListener("resize", this.checkscreen);
+  },
+  methods: {
+    checkscreen() {
+      this.windowWidth = window.innerWidth;
+      if (this.windowWidth >= 767) {
+        return;
+      }
+      this.showPopup();
+      // this.closePopup()
+    },
+    showPopup() {
+      this.windowWidth = window.innerWidth;
+      if (this.windowWidth >= 767) {
+        return;
+      }
+      let secImg = document.querySelectorAll(".sec_img");
+      for (let i of secImg) {
+        i.addEventListener("click", () => {
+          i.classList.add("active");
+        });
+      }
+    },
+    closePopup() {
+      this.windowWidth = window.innerWidth;
+      if (this.windowWidth >= 767) {
+        return;
+      }
+
+      let closeIcon = document.querySelectorAll(".close-icon");
+
+      for (let i of closeIcon) {
+        i.addEventListener("click", (e) => {
+          e.stopPropagation();
+
+          let secImgActive = document.querySelector(".sec_img.active");
+          if (secImgActive) {
+            secImgActive.classList.remove("active");
+          }
+        });
+      }
+    },
+  },
+};
+</script>
+
