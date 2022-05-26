f180389d96790965df697170309adbdb9b48d746
surindermdb <surinderkumar.mdb@gmail.com>
2022-05-26 10:20:28 +0530

    changes for product out of stock, show product left

--- src/vue/components/render/collection/main-collection.vue
+++ src/vue/components/render/collection/main-collection.vue
@@ -235,27 +235,35 @@
             <div class="product_grid">
                 <div class="grid_inner" v-bind:class="{ grid_inner_max: gridMax, grid_inner_min: gridMin }">
                     <div class="centeralign addmargin" v-for="product in productList" :key="product.id">
-                        <div class="card" v-if="product.images.length>0" @mouseenter="productId=product.id" @mouseleave="productId=0">
-                            <div v-if="product.variants[0].featured_image!=null">
-                                <img :src="product.variants[0].featured_image.src" :id="product.id"
-                                 />
-                            </div>
-                            <div v-else>
-                                <img :src="product.images[0].src" :id="product.id"
+                        <div class="card" v-if="product.images.length>0" @mouseenter="productId=product.id" @mouseleave="cardmouseleave(product.id)">
+                            <div class="item_left" v-bind:class="{ item_left_active: product.id == productId }" :id="'item_left_'+product.id">{{product.variants[0].stock<=5?'ONLY '+ product.variants[0].stock +' LEFT':''}}</div>
+                            
+                            <div class="product_img_wrapper" :id="'product_img_wrapper'+product.id" v-bind:class="{ out_of_stock: product.variants[0].stock == 0 }">
+                                <img v-if="product.variants[0].featured_image!=null" :src="product.variants[0].featured_image.src" :id="product.id" />
+                                <img  :src="product.images[0].src" :id="product.id"
                                 @mouseenter="mouseover($event, product.images[product.images.length - 1].src)"
-                                @mouseleave="mouseleave($event, product.images[0].src)" />
+                                @mouseleave="mouseleave($event, product.images[0].src)" v-else />
+                                <div class="out_of_stock_text" :id="'out_of_stock_text'+product.id" v-bind:class="{ out_of_stock_text_active: product.variants[0].stock == 0 }">Out Of Stock</div>
                             </div>
+                            <!-- <div class="product_img_wrapper" v-bind:class="{ out_of_stock: product.variants[0].stock == 0 }" v-else>
+                                <img  :src="product.images[0].src" :id="product.id"
+                                @mouseenter="mouseover($event, product.images[product.images.length - 1].src)"
+                                @mouseleave="mouseleave($event, product.images[0].src)" v-else />
+                                <div class="out_of_stock_text" v-bind:class="{ out_of_stock_text_active: product.variants[0].stock == 0 }">Out Of Stock</div>
+                                
+                            </div> -->
                             <h5 class="card-title">{{ product.title }}</h5>
                             <h5 class="card-title bold">${{ Math.floor(product.variants[0].price) }}</h5>
                             <div class="quickButton" v-bind:class="{ quickActive: product.id == productId }">
                                 <ul >
                                     <li :key="color+index" class="nav-dots" v-for="color in product.options">
                                         <span v-if="color.name.toLowerCase().includes('color')">
-                                            <label for="img-1" :key="colors" class="nav-dot" id="img-dot-1" @click="onSelectColor(colors,product)" v-for="colors in color.values"></label>
+                                            <!-- <button :key="colors" type="button" class="product-form__color-swatch navigable selected" v-for="colors in color.values" @click="onSelectColor(colors,product)" :style="inlineBgImage(colors)" tabindex="0" :aria-label="colors"></button> -->
+                                            <label for="img-1" :key="colors" class="nav-dot" :style="inlineBgImage(colors)" id="img-dot-1" @click="onSelectColor(colors,product)" v-for="colors in color.values"></label>
                                         </span>
                                     </li>
                                 </ul>
-                                <button class="quickAdd" @click="addToCard(product)">
+                                <button :id="'quickAdd'+product.id" class="quickAdd" v-bind:class="{ quickAdd_deactive: product.variants[0].stock == 0 }" @click="addToCard(product)">
                                     <span>Quick Add</span>
                                 </button>
                             </div>
@@ -364,9 +372,28 @@ export default {
     },
     methods: {
 
+        cardmouseleave:function(id){
+            this.productId=0;
+            let quickAdd=document.querySelector('#quickAdd'+id);
+            quickAdd.classList.remove("quickAdd_active");
+        },
+        
+        inlineBgImage:function(colors){
+            let color=colors.toLowerCase().replace(/[^A-Z0-9]+/ig, "-");
+            let bgImage = "//cdn.shopify.com/s/files/1/0899/2182/files/" + color + ".png?v=5251390435792914590";
+
+            let url=`background-image:url(${bgImage});`
+            return url + 'background:'+ color;
+        },
+
         onSelectColor:function(color,product){
             console.log(product.id);
             let img=document.getElementById(product.id);
+            let div=document.querySelector('#item_left_'+product.id);
+            let product_img_div=document.querySelector('#product_img_wrapper'+product.id);
+            let out_of_stock_text=document.querySelector('#out_of_stock_text'+product.id);
+            let quickAdd=document.querySelector('#quickAdd'+product.id);
+             
             console.log(img);
             let varints= product.variants.filter(item=>{
                 return item.title.includes(color)
@@ -376,8 +403,22 @@ export default {
                     console.log(varints[0].featured_image.src);
                     img.src=varints[0].featured_image.src;
                 }
+                div.textContent=varints[0].stock<=5?'ONLY '+varints[0].stock+' LEFT':'';
+                if(varints[0].stock == 0){
+                    out_of_stock_text.classList.add("out_of_stock_text_active");
+                    product_img_div.classList.add("out_of_stock");
+                    quickAdd.classList.remove("quickAdd_active");
+                    quickAdd.classList.add("quickAdd_deactive");
+                    
+                }
+                else{
+                    out_of_stock_text.classList.remove("out_of_stock_text_active");
+                    product_img_div.classList.remove("out_of_stock");
+                    quickAdd.classList.remove("quickAdd_deactive");
+                    quickAdd.classList.add("quickAdd_active");
+                }
+                
             }
-            console.log(color);
         },
 
         /* change images on hover */
@@ -1412,7 +1453,12 @@ h4 {
     gap: 15px;
     flex-direction: row;
     align-items: flex-start;
-    
+}
+.quickAdd_deactive{
+    visibility: hidden;
+}
+.quickAdd_active{
+    visibility: visible;
 }
 .quickAdd span{
     font-style: normal;
@@ -1450,8 +1496,8 @@ h4 {
 
 .nav-dots .nav-dot {
   top: -5px;
-  width: 11px;
-  height: 11px;
+  width: 20px;
+  height: 20px;
   margin: 0 4px;
   position: relative;
   border-radius: 100%;
@@ -1480,5 +1526,50 @@ input#img-6:checked ~ .nav-dots label#img-dot-6 {
 }
 .AZURE{
     background: #007fff;
+}
+.product-form__color-swatch{
+    width: 50px;
+    height: 50px;
+    border-radius: 100%;
+    margin: 4px;
+}
+.item_left{
+    text-align: end;
+    height: 12px;
+
+    font-style: normal;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 12px;
+    align-items: center;
+    letter-spacing: 0.02em;
+
+    color: #878787;
+    visibility: hidden;
+}
+.out_of_stock img{
+    opacity: 0.2;
+}   
+.item_left_active{
+    visibility: visible !important;
+}
+.product_img_wrapper{
+    position: relative;
+}
+.out_of_stock_text{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 12px;
+    align-items: center;
+    letter-spacing: 0.02em;
+    color: red;
+    transform: translate(-50%,-50%);
+    visibility: hidden;
+}
+.out_of_stock_text_active{
+    visibility: visible !important ;
 }
 </style>
