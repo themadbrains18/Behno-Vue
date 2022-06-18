9f88af3342071a1ba4d633904236f8920fc2ca92
sunnymasih0253 <sunnyk.mdb@gmail.com>
2022-06-18 16:37:10 +0530

    update code of blog page

--- src/vue/components/render/blog/shop-all-bags.vue
+++ src/vue/components/render/blog/shop-all-bags.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="all_feature_bag_sec ">
+    <section class="all_feature_bag_sec" v-if="shopifyData.showSection == 'true'">
         <div class="container">
         <h3 class="card_heading">{{shopifyData.sectionHeading}}</h3>
         <!-- Sec Content -->
@@ -13,41 +13,10 @@
                             <p class="subtitle_b">{{item.product.productidPrice}}</p>
                         </div>
                     </a>
-                    <div class="product_varients">
-                        <div class="product_colores">
-                            <ul>
-                                <li class="color">
-                                    <img src="https://cdn.shopify.com/s/files/1/0577/1178/8125/files/yellow.png?v=1651228173" alt="error">
-                                </li>
-                                <li class="color">
-                                    <img src="https://cdn.shopify.com/s/files/1/0577/1178/8125/files/golden.png?v=1651228172" alt="error">
-                                </li>
-                                <li class="color">
-                                    <img src="https://cdn.shopify.com/s/files/1/0577/1178/8125/files/white.png?v=1651228173" alt="error">
-                                </li>
-                                <li class="color">
-                                    <img src="https://cdn.shopify.com/s/files/1/0577/1178/8125/files/skyblue.png?v=1651228172" alt="error">
-                                </li>
-                                <li class="color">
-                                    <img src="https://cdn.shopify.com/s/files/1/0577/1178/8125/files/black.png?v=1651228172" alt="error">
-                                </li>
-                                <li class="color">
-                                    <img src="https://cdn.shopify.com/s/files/1/0577/1178/8125/files/red_purple.png?v=1651228172" alt="error">
-                                </li>
-                            </ul>
-                            
-                                <svg class="next_arrow" width="6" height="11" viewBox="0 0 6 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M0.588135 10.0294L4.9999 5.61765L0.588135 1.20589" stroke="#6B6B6B" stroke-linejoin="round"/>
-                                </svg>
-                            
-                        </div>
-                        <div class="product_btn">
-                            <a href="#" class="cta_btn">{{shopifyData.addfeatureBagBtn}}</a>
-                        </div>
-                    </div>
                 </div>
             </div>
             <div class="product_cards tmb-md-none">
+                
                 <div class="card" v-for="(item, index) in shopifyData.productData.slice(4,8)" :key="index">
                         <a :href=(item.product.productUrl)  :product-id=(item.product.productid) class="product_link" >
                             <img :src="item.product.productidImage"  />
@@ -56,38 +25,6 @@
                                 <p class="subtitle_b">{{item.product.productidPrice}}</p>
                             </div>
                         </a>
-                    <div class="product_varients">
-                        <div class="product_colores">
-                            <ul>
-                                <li class="color">
-                                    <img src="https://cdn.shopify.com/s/files/1/0577/1178/8125/files/yellow.png?v=1651228173" alt="error">
-                                </li>
-                                <li class="color">
-                                    <img src="https://cdn.shopify.com/s/files/1/0577/1178/8125/files/golden.png?v=1651228172" alt="error">
-                                </li>
-                                <li class="color">
-                                    <img src="https://cdn.shopify.com/s/files/1/0577/1178/8125/files/white.png?v=1651228173" alt="error">
-                                </li>
-                                <li class="color">
-                                    <img src="https://cdn.shopify.com/s/files/1/0577/1178/8125/files/skyblue.png?v=1651228172" alt="error">
-                                </li>
-                                <li class="color">
-                                    <img src="https://cdn.shopify.com/s/files/1/0577/1178/8125/files/black.png?v=1651228172" alt="error">
-                                </li>
-                                <li class="color">
-                                    <img src="https://cdn.shopify.com/s/files/1/0577/1178/8125/files/red_purple.png?v=1651228172" alt="error">
-                                </li>
-                            </ul>
-                            
-                                <svg class="next_arrow" width="6" height="11" viewBox="0 0 6 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M0.588135 10.0294L4.9999 5.61765L0.588135 1.20589" stroke="#6B6B6B" stroke-linejoin="round"/>
-                                </svg>
-                            
-                        </div>
-                        <div class="product_btn">
-                            <a :href=(item.product.productUrl)  :product-id=(item.product.productid) class="cta_btn">{{shopifyData.addfeatureBagBtn}}</a>
-                        </div>
-                    </div>
                 </div>
             </div>
             <div class="cta_link t-center">
@@ -146,46 +83,7 @@
         color: #fff;
         border: 1px solid transparent;
     }
-    /* product colors selector Start */
-    .product_colores,
-    .product_colores > ul
-    {
-        display: flex;
-        align-items: center;
-        gap: 11px;
-    }
-    .product_colores > ul > li{
-        line-height: 0;
-    }
-    .product_varients {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        gap: 20px;
-        opacity: 0;
-        visibility: hidden;
-        transition: 0.4s;
-    }
-    .next_arrow,
-    .color img
-    {
-        cursor: pointer;
-    }
-    .color img{
-        border: 1px solid #E0E0E0;
-        border-radius: 50%;
-    }
-    .card > a:hover + .product_varients ,
-    .product_varients:hover
-    {
-        opacity: 1;
-        visibility: visible;
-    }
-    
-    .sec_content::-webkit-scrollbar {
-        display: none;
-    }
-    /* product colors selector End */
+   
 
     /* Responsive Breakpoints */
     @media(max-width:1350px){
